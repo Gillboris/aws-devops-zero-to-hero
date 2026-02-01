9b351f4a1486f56e9c7d411c652ceb4ae330837a
Gillboris <messahodikpe@yahoo.com>
2026-01-31 23:36:47 -0500

    deployment

--- day-14/simple-python-app/start_container.sh
+++ day-14/simple-python-app/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker
+docker pull agbanvi/simple-python-flask-app
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 agbanvi/simple-python-flask-app
